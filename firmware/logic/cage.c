2b9b55eb4283fda49b01655ea593921103a59c1d
Eric Allen <ericpallen@gmail.com>
2010-02-01 18:59:35 -0500

    Successfully entering and exiting command mode
    Confirmed that as_start() makes cage_main() get called far more often

--- firmware/logic/cage.c
+++ firmware/logic/cage.c
@@ -13,6 +13,10 @@
 #define CAGE_MODE_SLEEP     0x01
 #define CAGE_MODE_COMMAND   0x02
 
+#define FACING_DOWN (xyz[2] > 0xE0 && xyz[2] < 0xE8)
+
+#define COMMAND_EXIT_COUNT 100
+
 void cage_main(void){
   static u8 xyz[3];
   static u8 down_count = 0;
@@ -29,13 +33,15 @@ void cage_main(void){
     Timer0_A4_Delay(CONV_MS_TO_TICKS(20));
     as_get_data(xyz);
 
-    if(xyz[2] > 0xE0 && xyz[2] < 0xE8){
+    if(FACING_DOWN){
       // facing down
       down_count += 1;
       if(down_count >= 2){
         down_count = 0;
         // go to command mode
+        start_buzzer(3, CONV_MS_TO_TICKS(20), CONV_MS_TO_TICKS(150));
         mode = CAGE_MODE_COMMAND;
+        return;
       }
     }
     else
@@ -45,12 +51,27 @@ void cage_main(void){
     break;
 
     case CAGE_MODE_COMMAND:
+    as_get_data(xyz);
+    
+    if(xyz[2] > 0xCF && xyz[2] < 0xD4){
+      ++down_count;
+      if(down_count > COMMAND_EXIT_COUNT){
+        down_count = 0;
+        as_stop();
+        display_symbol(LCD_ICON_RECORD, SEG_OFF);
+        start_buzzer(3, CONV_MS_TO_TICKS(20), CONV_MS_TO_TICKS(150));
+        mode = CAGE_MODE_SLEEP;
+        return;
+      }
+    }
+    else
+      down_count = 0;
+      
     if(heartbeat)
       display_symbol(LCD_ICON_RECORD, SEG_OFF);
     else
       display_symbol(LCD_ICON_RECORD, SEG_ON);
     heartbeat = !heartbeat;
-    // TODO
     break;
   }
 }
