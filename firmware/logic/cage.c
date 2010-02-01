9efe0c5cc818573485082e8fc96f94f784366837
Eric Allen <ericpallen@gmail.com>
2010-02-01 14:06:12 -0800

    Simple mode switching and command mode
    Untested. When in command mode, cage_main() should get called every time the accelerometer has new data.

--- firmware/logic/cage.c
+++ firmware/logic/cage.c
@@ -9,19 +9,48 @@
 #include "display.h"
 #include "timer.h"
 
-// from acceleration.c
-extern u16 convert_acceleration_value_to_mgrav(u8 value);
+#define CAGE_MODE_INIT      0x00
+#define CAGE_MODE_SLEEP     0x01
+#define CAGE_MODE_COMMAND   0x02
 
 void cage_main(void){
   static u8 xyz[3];
+  static u8 down_count = 0;
+  static u8 mode = CAGE_MODE_INIT;
+  static u8 heartbeat = 0;
 
-  as_start();
-  Timer0_A4_Delay(CONV_MS_TO_TICKS(20));
-  as_get_data(xyz);
-  as_stop();
-  
-  if(xyz[2] > 0xE0 && xyz[2] < 0xE8)
-    display_symbol(LCD_ICON_RECORD, SEG_ON);
-  else
-    display_symbol(LCD_ICON_RECORD, SEG_OFF);
+  switch(mode){
+    case CAGE_MODE_INIT:
+    mode = CAGE_MODE_SLEEP;
+    break;
+
+    case CAGE_MODE_SLEEP:
+    as_start();
+    Timer0_A4_Delay(CONV_MS_TO_TICKS(20));
+    as_get_data(xyz);
+
+    if(xyz[2] > 0xE0 && xyz[2] < 0xE8){
+      // facing down
+      down_count += 1;
+      if(down_count >= 2){
+        down_count = 0;
+        // go to command mode
+        mode = CAGE_MODE_COMMAND;
+      }
+    }
+    else
+      down_count = 0;
+    // don't stop if we're going to command mode
+    if(mode == CAGE_MODE_SLEEP) as_stop();
+    break;
+
+    case CAGE_MODE_COMMAND:
+    if(heartbeat)
+      display_symbol(LCD_ICON_RECORD, SEG_OFF);
+    else
+      display_symbol(LCD_ICON_RECORD, SEG_ON);
+    heartbeat = !heartbeat;
+    // TODO
+    break;
+  }
 }
