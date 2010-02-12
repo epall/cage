62151cf18734d8609fa1d43fc0df42eb2e9cd8d6
Eric Allen <ericpallen@gmail.com>
2010-02-11 22:16:39 -0500

    Find OSX path for port if COM4 not found

--- software/src/MainWindow.java
+++ software/src/MainWindow.java
@@ -1,3 +1,4 @@
+import gnu.io.NoSuchPortException;
 import org.jruby.embed.PathType;
 import org.jruby.embed.ScriptingContainer;
 
@@ -23,28 +24,34 @@ public class MainWindow {
     private JEditorPane AppScriptPane;
     private JButton StopAccelerometerButton;
     private Usbmodem dongle;
-    
+
 
     public MainWindow() {
+        this.dongle = new Usbmodem();
 
-    StartAccelerometer.addActionListener(new ActionListener() {
+        StartAccelerometer.addActionListener(new ActionListener() {
             public void actionPerformed(final ActionEvent e) {
                 try {
-                dongle.connect("COM4");
+                    dongle.connect("COM4");
                 }
-                catch(Exception ex) {
-                    ex.printStackTrace();
+                catch(Exception ex1){
+                    try {
+                        dongle.connect("/dev/tty.usbmodem001");
+                    }
+                    catch(Exception ex2) {
+                        JOptionPane.showMessageDialog(null, "Unable to connect to dongle");
+                    }
                 }
             }
         });
 
-      StopAccelerometerButton.addActionListener(new ActionListener() {
+        StopAccelerometerButton.addActionListener(new ActionListener() {
             public void actionPerformed(final ActionEvent e) {
                 dongle.closePort();
             }
         });
 
-      newGesture.addActionListener(new ActionListener() {
+        newGesture.addActionListener(new ActionListener() {
             public void actionPerformed(final ActionEvent e) {
 
             }
@@ -58,18 +65,14 @@ public class MainWindow {
 
     }
 
-    public void setDongle(Usbmodem dongle) {
-        this.dongle = dongle;
-    }
-
     public static void main(String[] args) {
         final JFrame window = new JFrame("CAGE");
         final MainWindow main = new MainWindow();
         window.setBounds(100, 100, 500, 400);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setContentPane(main.mainPanel);
-        SwingUtilities.invokeLater(new Runnable(){
-            public void run(){
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
                 window.setVisible(true);
             }
         });
