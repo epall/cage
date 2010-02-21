cdcf21f11f75a28fc9de514a0eb97effc145dbfc
Eric Allen <ericpallen@gmail.com>
2010-02-21 13:56:37 -0500

    Gesture plotter

---
+++ software/src/Gesture.java
@@ -0,0 +1,11 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: epall
+ * Date: Feb 21, 2010
+ * Time: 1:25:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Gesture {
+    public AccelerometerPoint getPoint(int index);
+    public int numPoints();
+}
