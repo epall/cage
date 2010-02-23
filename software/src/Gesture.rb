3d3fece72050438ae2e6ef3bbbb1f811e28d2c48
Eric Allen <ericpallen@gmail.com>
2010-02-23 13:39:21 -0500

    Fix some minor indentation issues

--- software/src/Gesture.rb
+++ software/src/Gesture.rb
@@ -1,35 +1,35 @@
 require 'src/recognizers/dollar2d'
 
 class Gesture
-    include Java::Gesture
-    include Dollar2D
+  include Java::Gesture
+  include Dollar2D
 
-	def initialize
-      @points = Array.new
-	end
+  def initialize
+    @points = Array.new
+  end
 
-	def add_point (point)
-        @points << point
-    end
+  def add_point (point)
+    @points << point
+  end
 
-    def addaction(action)
-      @action = action
-    end
+  def addaction(action)
+    @action = action
+  end
 
-    def convert_points_to_gesture
-      $stderr.puts @points.length
-      @resampled_points = points_to_gesture(@points)
-    end
+  def convert_points_to_gesture
+    $stderr.puts @points.length
+    @resampled_points = points_to_gesture(@points)
+  end
 
-	def returnallpoints
-	  return @points
-    end
+  def returnallpoints
+    return @points
+  end
 
-    def numPoints
-      return @points.count
-    end
+  def numPoints
+    return @points.count
+  end
 
-    def getPoint(index)
-      return @points[index]
-    end
+  def getPoint(index)
+    return @points[index]
+  end
 end
