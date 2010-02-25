2c53c95e3d3107f0d42cda8d4e6702c9323c4857
Michael O'Keefe <okeefm@rpi.edu>
2010-02-25 11:00:06 -0500

    changed some functions to accessors and writers

--- software/src/Gesture.rb
+++ software/src/Gesture.rb
@@ -12,27 +12,29 @@ class Gesture
     @points << point
   end
 
-  def addaction(action)
-    @action = action
-  end
-
   def convert_points_to_gesture
     $stderr.puts @points.length
     @resampled_points = points_to_gesture(@points)
   end
 
-  def numPoints
+  def num_points
     return @points.count
   end
 
-  def getPoint(index)
+  def get_point (index)
     return @points[index]
+  end
+
+  def do_action
+    
   end
 
   def test_gesture(test_gestures)
     t_prime, score = recognize(@resampled_points, test_gestures)
-    #t_prime is the gesture from test_gestures that is the best match, score is the score (0-1) of that match
+    #t_prime is the gesture from test_gestures that is the best match, score is the score of that match
+    $stderr.puts "#{t_prime.action} is the best recognized gesture, with a score of #{score}"
   end
 
-  attr_accessor :points, :resampled_points
+  attr_reader :points, :resampled_points
+  attr_accessor :action
 end
