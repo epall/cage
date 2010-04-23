9ed179a79e9f72c6cd30e5c7fb5ac1f8e42cda9e
Michael O'Keefe <okeefm@rpi.edu>
2010-04-23 09:59:29 -0400

    Added a method to the Ruby point class to return an array of the acceleration vals

--- software/src/point.rb
+++ software/src/point.rb
@@ -7,5 +7,13 @@ class Point
   end
 
   attr_accessor :x, :y, :z
+
+  def int_array ()
+    an_array = Array.new(3)
+    an_array[0] = @x
+    an_array[1] = @y
+    an_array[2] = @z
+    return an_array
+  end
   
 end
